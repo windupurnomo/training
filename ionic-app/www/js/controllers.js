9c1e55e20e237d3b0e1c456c53d761e8ccc6b9c0
Windu Purnomo <wpwindupurnomo@gmail.com>
2016-05-13 13:13:50 +0700

    bug fixing, create

--- ionic-app/www/js/controllers.js
+++ ionic-app/www/js/controllers.js
@@ -1,9 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
-
-})
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {})
 
 .controller('UsersCtrl', function($scope, $http) {
     var url = "http://localhost:8080/api/user";
@@ -13,13 +10,26 @@ angular.module('starter.controllers', [])
     })
 })
 
-.controller('UserCtrl', function($scope, $stateParams, $http) {
+.controller('UserCtrl', function($scope, $state, $stateParams, $http) {
     var url = "http://localhost:8080/api/user";
     var id = $stateParams.userId;
-    var isCreate = id === -1;
+    var isCreate = id == -1;
     if (!isCreate) {
         $http.get(url + "/" + id).then(function(res) {
             $scope.user = res.data;
         });
+    }
+
+    $scope.save = function (){
+    	var data = angular.copy($scope.user);
+    	if (isCreate){
+    		$http.post(url, data).then(success);
+    	}else{
+    		$http.put(url, data).then(success);
+    	}
+    }
+
+    var success = function (){
+    	$state.go("app.users");
     }
 });
